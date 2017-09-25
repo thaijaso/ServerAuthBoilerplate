45766edf69c03915f6f7e19daff7feb608086707
Thai <Jason.Thai3@T-Mobile.com>
2017-09-25 19:42:16 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const router = require('./router');
+
+// DB Setup
+mongoose.connect('mongodb://localhost:authentication/authentication');
+
+// App Setup
+const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
+
+// Server Setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on:', port);
